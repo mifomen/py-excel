6ecd804ce81d1b093750d31c22f30b95ac2f2a63
Ilya Koshelev <mifomen@yandex.ru>
2023-04-19 20:32:35 +0300

    Master

--- main.py
+++ main.py
@@ -2,51 +2,22 @@
 # %appdata%  Roaming\gspread\service_account.json
 # запрос на указание файла с данными
 import time
-from tkinter.filedialog import askopenfilename
+# from tkinter.filedialog import askopenfilename
 from datetime import datetime
 import openpyxl
 import gspread  # pip install openpyxl gspread
 
+# import pip
+# pip.main(["install", "gspread"])
+# pip.main(["install", "openpyxl"])
 
-import pip
-pip.main(["install", "gspread"])
-pip.main(["install", "openpyxl"])
+URL = "orders_monitoring_2023_04_18_13_07_20364910.xlsx"
 
-
-# from imp import find_module
-
-# def checkPythonmod(mod):
-#     try:
-#         op = find_module(mod)
-#         return True
-#     except ImportError:
-#         return False
-# try:
-#   import openpyxl
-# except ImportError as e:
-#   pip.main(["install", "openpyxl"])
-#   import openpyxl
-
-# try:
-#   import gspread
-# except ImportError as e:
-#   pip.main(["install", "gspread"])
-#   import gspread
-
-# if python -c 'import pkgutil; exit(not pkgutil.find_loader("pandas"))'; then
-#     echo 'pandas found'
-# else
-#     echo 'pandas not found'
-# fi
+# запрос на указание файла с данными
+# URL = askopenfilename()
 
 # starting time
 start = time.time()
-
-
-# URL = "orders_monitoring_2023_04_18_13_07_20364910.xlsx"
-
-# запрос на указание файла с данными
-URL = askopenfilename()
 
 # FILE_NAME = 'test.xlsx'
 # wb = openpyxl.reader.excel.load_workbook(filename=FILE_NAME,data_only=True)
@@ -60,25 +31,27 @@ sheetLocal = wb.active
 
 
 # функция чтобы найти число в конце строки
-def atlestCharToInt(cellValue):
+def atlestCharToInt(сellValue):
     # если применяем к пустой строке,
-    """Function printing python version."""
-    if (cellValue == '') or (cellValue == 0) or (cellValue is None):
-        SS = 0
-        return SS  # то вернуть пустую строку
-    # lengthLine = len(cellValue.velue)
+    """Извлечь цифры из конца данных ячейки"""
+    if (len(сellValue) <= 1) or (сellValue == 0) or (сellValue is None):
+        # SS = 0
+        return 0  # то вернуть пустую строку
+    # lengthLine = len(сellValue.velue)
     # забираем 3 последних символа, оберзаем пробелы
-    SS = cellValue[len(cellValue)-3:len(cellValue)].strip()
+    # SS = сellValue[len(сellValue)-3:len(сellValue)].strip()
     # полученную строку переводим в числовой тип данных
-    return int(SS)
+    lengthСell = len(сellValue)
+    return int(сellValue[lengthСell-3:lengthСell].strip())
 
 # a = "Питающихся: 25Комплекты:- Завтрак 1-4 класс: 25"
 # # СТрока для тестов нахождения последних символов
-# s = atlestCharToInt(a); # результат функции для нахождения последних цифры в строке
+# s = atlestCharToInt(a); # результат функции для
+# нахождения последних цифры в строке
 
 
-def split(arr, size):  # функция для разбиения листа на под листы, чтбоы отправить на страницу
-    """Function printing python version."""
+def split(arr, size):
+    """Функция для разбиения листа на под листы, чтобы отправить на страницу"""
     arrs = []
     while len(arr) > size:
         pice = arr[:size]
@@ -98,48 +71,32 @@ sh = gc.open("Питание Лицей")
 # Выбрать ппервый активный у нее лист
 GoogleSheets = sh.sheet1
 
-array = []
-for i in range(50, 54, 1):  # формирование обед и полдник у 2АБВГ
-    array.append(atlestCharToInt(sheetLocal['D' + str(i)].value))
-    array.append(atlestCharToInt(sheetLocal['E' + str(i)].value))
-# пакуем в подлисты по 2 для вставки в гугл таблицу
-x = split(array, 2)
 
-array = []
-for i in range(5, 18, 1):  # завтраки 1АБВГД, 2АБВГ 3АБВГ класс
-    array.append(atlestCharToInt(sheetLocal['C' + str(i)].value))
-y = split(array, 1)
+def cellToData(rangeStart, rangeStop, charSheet):
+    """Перенос данных из локальной xlsx в память питона"""
+    array = []
+    for i in range(rangeStart, rangeStop, 1):
+        # завтраки 1АБВГД, 2АБВГ 3АБВГ класс
+        array.append(atlestCharToInt(sheetLocal[str(charSheet) + str(i)].value))
+    # y = split(array, 1)
+    return split(array, 1)
 
-array = []
-for i in range(18, 22, 1):  # обеды 4АБВГ класс
-    array.append(atlestCharToInt(sheetLocal['D' + str(i)].value))
-z = split(array, 1)
 
-array = []
-for i in range(22, 26, 1):  # завтраки 5АБВГ класс
-    array.append(atlestCharToInt(sheetLocal['C' + str(i)].value))
-v = split(array, 1)
-
-array = []
-for i in range(26, 33, 1):  # обеды 6АБВГ 7АБВ класс
-    array.append(atlestCharToInt(sheetLocal['D' + str(i)].value))
-b = split(array, 1)
-
-array = []
-for i in range(33, 36, 1):  # завтраки 8АБВ класс
-    array.append(atlestCharToInt(sheetLocal['C' + str(i)].value))
-n = split(array, 1)
-
-array = []
-for i in range(36, 45, 1):  # завтраки 9АБВ 10АБВ 11АБВ класс
-    array.append(atlestCharToInt(sheetLocal['C' + str(i)].value))
-nn9 = split(array, 1)
+x1 = cellToData(50, 54, 'D')  # формирование обед у 2АБВГ
+x2 = cellToData(50, 54, 'E')  # формирование полдник у 2АБВГ
+y = cellToData( 5, 18, 'C')  # завтраки 1АБВГД, 2АБВГ 3АБВГ класс
+z = cellToData(18, 22, 'D')  # обеды 4АБВГ класс
+v = cellToData(22, 26, 'C')  # завтраки 5АБВГ класс
+b = cellToData(26, 33, 'D')  # обеды 6АБВГ 7АБВ класс
+n = cellToData(33, 36, 'C')  # завтраки 8АБВ класс
+nn9 = cellToData(36, 45, 'C')  # завтраки 9АБВ 10АБВ 11АБВ класс
 
 
 gg = []  # формирование обед и полдник у 1АБВГД
 classLastNames = ['А', 'Б', 'В', 'Г', 'Д']
 
-for i in classLastNames:  # созданеи нужной последовательности ГПД в 1-х классах
+for i in classLastNames:
+    # созданеи нужной последовательности ГПД в 1-х классах
     for j in range(45, 50, 1):
         s = sheetLocal['B' + str(j)].value
         if s[len(s)-1] == i:
@@ -149,8 +106,8 @@ for i in classLastNames:  # созданеи нужной последовате
             gg.append(XY)
             break
 
-gg = split(gg, 2)
-# print(f"gg = {gg}")
+gg = split(gg, 2) # сохраненный обед и полдник у 1АБВГД
+
 
 currentDay = datetime.now().day
 if currentDay <= 10:
@@ -161,17 +118,15 @@ if currentMonth <= 10:
 
 currentYear = datetime.now().year
 today = f'{currentDay}.{currentMonth}.{currentYear}'
-#
-# today = "=TODAY()"
 
-# print('today ' + str(today))
-# today = split(today,1)
 # Ввод информации в гугл таблицу
-
 GoogleSheets.update("E1", today)
 GoogleSheets.batch_update([{
-    'range': 'C8:D11',  # диапазон куда грузим
-    'values': x,  # загружаем обед и полдник у 2АБВГ
+    'range': 'C8:C11',  # диапазон куда грузим
+    'values': x1,  # загружаем обед и полдник у 2АБВГ
+},{
+    'range': 'D8:D11',  # диапазон куда грузим
+    'values': x2,  # загружаем обед и полдник у 2АБВГ
 }, {
     'range': 'C3:D7',  # диапазон куда грузим
     'values': gg,  # загружаем обед и полдник у 1АБВГД
